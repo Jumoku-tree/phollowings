f8c14a47c6c3d9267a95f65e74040c9c04da2251
Jumoku-tree <102124101+Jumoku-tree@users.noreply.github.com>
2022-05-05 18:22:33 +0900

    投稿機能調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
+  has_many_attached :images
+  # belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
