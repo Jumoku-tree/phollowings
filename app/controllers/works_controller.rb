a11c9ed9e36edbce1c473429ae6575cbaab097e8
Jumoku-tree <102124101+Jumoku-tree@users.noreply.github.com>
2022-05-08 17:41:07 +0900

    マイページ機能（とりあえず最低限）

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,15 @@
 class WorksController < ApplicationController
-  require 'rubygems'
   require 'RMagick'
-  
+  include Magick
+
   def index
     @works = Work.all
+    @works.each do |work|
+      images = work.images
+      images.each do |image|
+        image.variant(gravity: :center, resize: "270x450^", crop: "270x450+0+0").processed
+      end
+    end
   end
 
   def new
@@ -21,13 +27,13 @@ class WorksController < ApplicationController
 
   def show
     @work = Work.find(params[:id])
-    @image_prev = Magick::Image.read(@work.images[0]).first
-    @image_next = Magick::Image.read(@work.images[1]).first
-
   end
 
   private
   def work_params
     params.require(:work).permit(:title, :caption, :category_id, { images: [] }).merge(user_id: current_user.id)
+  end
+
+  def resize_images
   end
 end
